12a5a450c7fdeed18e0e27a06e80b3a73498aeb8
Alexey Tsutsoev <alexeytsuts@gmail.com>
2025-03-02 01:25:24 +0200

    refactor(game): updated mock ap services

--- tic-tac-toe/Data/Game/MockGameApiService.swift
+++ tic-tac-toe/Data/Game/MockGameApiService.swift
@@ -2,23 +2,25 @@ import Foundation
 
 /// mock service for emulate async post game result.
 /// I haven't add any extra dependencies for real request.
-final class MockApiService {
+final class MockGameApiService {
     // MARK: - Example of Dependencies for common api service
-//    private let session: URLSession
-//    private let config: ApiServicesConfigProtocol
-//    private let routeManager: RouteManagerProtocol
+    //    private let session: URLSession
+    //    private let config: ApiServicesConfigProtocol
+    //    private let routeManager: RouteManagerProtocol
 
     // MARK: - Manual DI example without any DI libs
-//    init(session: URLSession, config: ApiServicesConfigProtocol, routeManager: RouteManagerProtocol) {
-//        self.session = session
-//        self.config = config
-//        self.routeManager = routeManager
-//    }
-    
+    //    init(session: URLSession, config: ApiServicesConfigProtocol, routeManager: RouteManagerProtocol) {
+    //        self.session = session
+    //        self.config = config
+    //        self.routeManager = routeManager
+    //    }
+
     /// mock method
     func postResult(win: Bool, date: Date = .now) async throws {
         /// emulate delay
-        try await Task.sleep(seconds: 2.0)
+        try await Task.sleep(seconds: 1.0)
         print("Result sent")
+        print("win? \(win)")
+        print("date is \(date)")
     }
 }
--- tic-tac-toe/Domain/Game/MockGameUseCase.swift
+++ tic-tac-toe/Domain/Game/MockGameUseCase.swift
@@ -1,8 +1,16 @@
-//
-//  MockGameRepository.swift
-//  tic-tac-toe
-//
-//  Created by Alexey Tsutsoev on 02.03.2025.
-//
-
 import Foundation
+
+/// mock use case for work with data layer
+final class MockGameUseCase {
+    /// I use api service without repository. for now
+    private let api: MockGameApiService
+
+    init(api: MockGameApiService) {
+        self.api = api
+    }
+
+    /// mock posing game result
+    func sendResult(win: Bool, date: Date = .now) async throws {
+        try await api.postResult(win: win, date: date)
+    }
+}
